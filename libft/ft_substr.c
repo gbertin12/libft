339c46ec190e1824134b0f89fcb2f84265c7e6d1
gbertin12 <gaelbertin12@gmail.com>
2022-01-03 09:00:09 +0100

    first commit

---
+++ libft/ft_substr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbertin <gbertin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/25 09:08:42 by gbertin           #+#    #+#             */
+/*   Updated: 2021/12/27 14:30:32 by gbertin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	unsigned int	i;
+	char			*dest;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	if (start > ft_strlen(s))
+		return (ft_strdup(""));
+	dest = (char *)malloc(len + 1);
+	if (!dest)
+		return (NULL);
+	while (i < len)
+	{
+		dest[i] = s[start + i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
